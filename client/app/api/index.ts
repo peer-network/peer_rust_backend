e2a9c1a6e05e9daca8aaa92c88fb81f9a90ee8f7
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-04-15 16:55:14 +0200

    initial server files

--- client/app/api/index.ts
+++ client/app/api/index.ts
@@ -1,5 +1,18 @@
-// Export client functions
-export * from './client';
+// import resolvers from './resolvers';
+import { ApolloServer } from 'apollo-server';
+import { readFileSync } from 'fs';
+import path from 'path';
+import resolvers from '../resolvers/resolvers';
 
-// Re-export commonly used types
-export { GraphQLClient, gql } from 'graphql-request'; 
+const schemaPath : string = "./schema.graphql"
+const typeDefs = readFileSync(path.join(__dirname, schemaPath), 'utf8');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Server starten
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+}); 
