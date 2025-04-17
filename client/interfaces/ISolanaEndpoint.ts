b431987080477ddff946f01aac06370c4914deec
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-04-17 10:18:14 +0200

    feat(client): add ISolanaEndpoint, SolanaEndpointImpl

---
+++ client/interfaces/ISolanaEndpoint.ts
@@ -0,0 +1,8 @@
+import { StoreAllResult } from "../../api_integration/app/types"
+import { DataItem } from "../../api_integration/app/types"
+
+export interface ISolanaEndpoint {
+  runIntegration() : void
+  storeAllData(userId : string) : Promise<StoreAllResult | undefined>
+  readingStoredDataFromSolana(storeResult : StoreAllResult) : Promise<DataItem | undefined>
+}
